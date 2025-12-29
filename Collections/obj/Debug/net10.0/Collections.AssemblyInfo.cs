8ccf8e5db4029ac5aba039ce1a5641bd70fddfa0
shadikodehode <shadi.kodehode@gmail.com>
2025-12-29 09:30:41 +0100

    BM1.2
    gitignore

--- Collections/obj/Debug/net10.0/Collections.AssemblyInfo.cs
+++ Collections/obj/Debug/net10.0/Collections.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collections")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0dec6aec08038e0c20d662872a87d3630cfccb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f79a5f9f6280f74d629909b62a2db544262954")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collections")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collections")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
