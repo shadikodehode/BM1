5076b9d7638d23ea61ea07f4c9fd79f5243c9924
shadikodehode <shadi.kodehode@gmail.com>
2026-02-02 09:27:39 +0100

    RandomDigimon.6
    Article helper addon

--- RandomDigimon/Program.cs
+++ RandomDigimon/Program.cs
@@ -8,11 +8,11 @@ class Program
 
   static void Main(string[] args)
   {
-   DigimonReader digiread = new();
-   var digiRandom = digiread.GetRandomDigimon();
-
-   Console.WriteLine($"Your digimon is {digiRandom.Name}! it is a {digiRandom.Stage}, of the attribute {digiRandom.Atr}"); //change structure
-   Console.WriteLine($"You got a {digiRandom.Atr} {digiRandom.Name}, it is a {digiRandom.Stage}");
+    DigimonReader digiread = new();
+    var digiRandom = digiread.GetRandomDigimon();
+    var article = ArticleHelper.ArticleH(digiRandom.Atr);
+   
+    Console.WriteLine($"You got {article} {digiRandom.Atr} {digiRandom.Name}, and is a {digiRandom.Stage}");
   }
 
 }
--- RandomDigimon/View/ArticleHelper.cs
+++ RandomDigimon/View/ArticleHelper.cs
@@ -2,15 +2,20 @@ using System;
 
 namespace RandomDigimon.View;
 
+/// <summary>
+/// An article helper to put a or an depending on if the pokemons name starts with a vowel or not 
+/// A simple binary method that works for general names, but would be too simple for a wider use case, 
+/// i.e if a sound starts with a vowel but is written with consonant
+/// </summary>
 public class ArticleHelper
 {
-    private static string ArticleH(string? word)
+    public static string ArticleH(string? word)
     {
         if(string.IsNullOrWhiteSpace(word))
         return "a";
 
         char first = char.ToLower(word[0]);
 
-        return "aeiou".Contains(first) ? "an" : "a";
+        return "aeiou".Contains(first) ? "an" : "a"; // "ifTrue" : "ifFalse", this is just a what if statement that is compacted
     }
 }
